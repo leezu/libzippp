ba9aec43f7760c3384bbe36f9a5454912f61307a
David Demelier <markand@malikania.fr>
2018-01-02 15:40:15 +0100

    Update copyrights

--- test/main.cpp
+++ test/main.cpp
@@ -1,7 +1,7 @@
 /*
  * main.cpp -- test the zip wrapper functions
  *
- * Copyright (c) 2013-2017 David Demelier <markand@malikania.fr>
+ * Copyright (c) 2013-2018 David Demelier <markand@malikania.fr>
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
